303a84081e15c04f634944f687833c1aa27b2233
hoi-82 <tmddus828@gmail.com>
2024-01-04 15:54:51 +0900

    Initial commit

---
+++ src/main/java/com/info/infoma/InfomaApplication.java
@@ -0,0 +1,13 @@
+package com.info.infoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/infoma/InfomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.infoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
